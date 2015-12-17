fdf608fdee07cf1178e86bfa6f0416d780da0f92
Nick Sutterer <apotonick@gmail.com>
2015-12-17 14:59:44 +1100

    it kinda works, but activity/email/create.rb still won't.

--- lib/trailblazer/loader.rb
+++ lib/trailblazer/loader.rb
@@ -9,35 +9,62 @@ module Trailblazer
     # NOTE: i will most probably use call_sheet and dry-container here soon.
     def call(options={}, &block)
       options[:concepts_root] ||= "app/concepts/"
+      options[:concept_dirs] = concept_dirs
 
       pipeline = options[:pipeline] || Representable::Pipeline[
+        FindDirectories,
         FindConcepts,
-        SortConcepts,
-        Representable::Collect[ConceptName, ConceptFiles] # per concept.
+        # PrintConcepts,
+        SortByLevel,
+        Representable::Collect[ConceptName, ConceptFiles, SortCreateFirst, SortOperationLast, AddConceptFiles] # per concept.
       ]
 
       if args = options[:insert] # FIXME: this only implements a sub-set.
         # pipeline = Representable::Pipeline::Insert.(pipeline, *args) # FIXME: implement :before in Pipeline.
-        pipeline[2].insert(pipeline[2].index(args.last[:before]), args.first)
+        pipeline[3].insert(pipeline[3].index(args.last[:before]), args.first)
       end
 
       files =  pipeline.([], options).flatten
 
-      # require "pp"
-      # pp files
+      require "pp"
+      pp files
 
       load_files(files, &block)
     end
 
-    FindConcepts  = ->(input, options) { Dir.glob("#{options[:concepts_root]}**/") }
-    # lame heuristic, but works for me: sort by nested levels.
+    def concept_dirs
+      %w{ callback cell contract operation policy representer view }
+    end
+
+    FindDirectories  = ->(input, options) { Dir.glob("#{options[:concepts_root]}**/") }
+    # Filter out all directories containing /(callback|cell|contract|operation|policy|representer|view)/
+    FindConcepts     = ->(input, options) { input.shift; input.reject { |dir| dir =~ /(#{options[:concept_dirs].join("|")})/ } }
+    PrintConcepts    = ->(input, options) { puts "  concepts: #{input.inspect}"; input }
+
+    # lame heuristic, but works for me: sort by directory levels.
     # app/concepts/comment
     # app/concepts/api/v1/comment
-    SortConcepts  = ->(input, options) { input.sort { |a, b| a.split("/").size <=> b.split("/").size } }
+    SortByLevel  = ->(input, options) { input.sort { |a, b| a.split("/").size <=> b.split("/").size } }
     # Extract concept name from path, e.g. /api/v1/comment.
     ConceptName   = ->(input, options) { options[:name] = input.sub(options[:concepts_root], "").chomp("/"); [] }
     # Find all .rb files in one particular concept directory, e.g. as in /concepts/comment/*.rb.
-    ConceptFiles  = ->(input, options) { input += Dir.glob("#{options[:concepts_root]}#{options[:name]}/*.rb") }
+    ConceptFiles  = ->(input, options) {
+      i = Dir.glob("#{options[:concepts_root]}#{options[:name]}/*.rb") +
+        Dir.glob("#{options[:concepts_root]}#{options[:name]}/*/*.rb")
+
+      ; puts "    files: #{i.inspect}"; i }
+
+    # operation files should be loaded after callbacks, policies, and the like: [callback.rb, contract.rb, policy.rb, operation.rb]
+    require "pp"
+    SortOperationLast = ->(input, options) {
+      # pp input
+
+    input.sort { |a, b| (a =~ /operation/ && b !~ /operation/) ? 1 : -1 } }
+    SortCreateFirst   = ->(input, options) { input.sort }
+    AddConceptFiles   = ->(input, options) {
+      pp input
+      input }
+
 
   private
 
